d65ad944ba1a30dc462873544ef9bd2f0f7e2095
AndZapata <andzapata514@gamil.com>
2019-09-27 22:58:41 +0000

    [Fix] add lines codes from point 1

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,24 @@
 $(function () {
-  $.get('http://localhost:5001/api/v1/status/', function (data) {
-    $('#api_status').toggleClass('available', data.status === 'OK');
-  });
+    const $ = window.$;
+    const inputsAmenities = $('.amenities input');
+    $(inputsAmenities).change(function () {
+	checkAmenities();
+    });
+    $.get('http://localhost:5001/api/v1/status/', function (data) {
+      $('#api_status').toggleClass('available', data.status === 'OK');
+    });
 });
+
+function checkAmenities () {
+    const amenitiesName = [];
+    const amenitiesId = [];
+    $('.amenities input:checked').each(function () {
+	amenitiesName.push($(this).attr('data-name'));
+	amenitiesId.push($(this).attr('data-id'));
+    });
+    if (amenitiesName.length === 0) {
+	$('.amenities h4').html('&nbsp;');
+    } else {
+	$('.amenities h4').text(amenitiesName.join(', '));
+    }
+}
