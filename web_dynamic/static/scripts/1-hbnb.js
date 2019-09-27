61bab4fb0d9c987ee079072b1ce0990db8fe6092
nildiert <niljordan23@gmail.com>
2019-09-27 21:23:24 +0000

    [FEATURE] Added 1-hbnb.js 1-hbnb.html 1-hbnb.js

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,22 @@
+$(function() {
+    inputsAmenities = $('.amenities input');
+    $(inputsAmenities).change( function(){
+	checkAmenities();
+    })
+});
+
+function checkAmenities () {
+    amenitiesName = [];
+    amenitiesId = [];
+    $(".amenities input:checked").each(function (){
+	amenitiesName.push($(this).attr("data-name"));
+	amenitiesId.push($(this).attr("data-name"));
+    });
+    if (amenitiesName.length === 0) {
+	$('.amenities h4').html('&nbsp;')
+    } else {
+	$('.amenities h4').text(amenitiesName.join(", "))	
+    }
+
+
+}
