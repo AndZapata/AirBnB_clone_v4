96b772e3ee2f847dbeca57e0208d5d5c9e73c4e2
nildiert <niljordan23@gmail.com>
2019-09-27 22:42:00 +0000

    [FIX] Fixed semistandard style

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,21 @@
-$(function() {
-    inputsAmenities = $('.amenities input');
-    $(inputsAmenities).change( function(){
-	checkAmenities();
-    })
+$(function () {
+  const $ = window.$;
+  const inputsAmenities = $('.amenities input');
+  $(inputsAmenities).change(function () {
+    checkAmenities();
+  });
 });
 
 function checkAmenities () {
-    amenitiesName = [];
-    amenitiesId = [];
-    $(".amenities input:checked").each(function (){
-	amenitiesName.push($(this).attr("data-name"));
-	amenitiesId.push($(this).attr("data-id"));
-    });
-    if (amenitiesName.length === 0) {
-	$('.amenities h4').html('&nbsp;')
-    } else {
-	$('.amenities h4').text(amenitiesName.join(", "))
-    }
-
-
+  const amenitiesName = [];
+  const amenitiesId = [];
+  $('.amenities input:checked').each(function () {
+    amenitiesName.push($(this).attr('data-name'));
+    amenitiesId.push($(this).attr('data-id'));
+  });
+  if (amenitiesName.length === 0) {
+    $('.amenities h4').html('&nbsp;');
+  } else {
+    $('.amenities h4').text(amenitiesName.join(', '));
+  }
 }
